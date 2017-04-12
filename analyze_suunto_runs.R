6b39ce7db408672f858f21164d8c1c884e88b685
Torsten Senf <sentor@sentor.de>
2017-04-12 21:12:06 +0200

    format duration in %H%M%S format ... some more metrics

--- analyze_suunto_runs.R
+++ analyze_suunto_runs.R
@@ -30,9 +30,15 @@ track$delta.dist = 0
 track$delta.dist[2:nrow(track)] = pointDistance(track[2:nrow(track), c("lon", "lat")], track[1:(nrow(track)-1), c("lon", "lat")], lonlat=TRUE)
 
 track$delta.time = 0
+## track$delta.time[2:nrow(track)] = as.numeric(difftime(track$time[2:nrow(track)], track$time[1:(nrow(track)-1)], units="secs"))
+
 track$delta.time[2:nrow(track)] = as.numeric(difftime(track$time[2:nrow(track)], track$time[1:(nrow(track)-1)], units="secs"))
 
 track$speed = 3.6 * track$delta.dist / track$delta.time
+
+# utc time ... get my time zone ... how?
+start_time = min(track$time[2:nrow(track)])
+finish_time = max(track$time[2:nrow(track)])
 
 ## print(track$speed)
 ## na.rm means remove NaN otherwise it's the result 
@@ -41,8 +47,21 @@ track$speed = 3.6 * track$delta.dist / track$delta.time
 ## print(mean(track$speed,na.rm=TRUE))
 
 my_mean_speed = mean(track$speed,na.rm=TRUE)
-my_time_minutes = sum(track$delta.time)/60
+my_max_speed = max(track$speed,na.rm=TRUE)
+my_time = sum(track$delta.time)
+my_time_hours = floor(my_time/3600)
+my_time_minutes =  floor((my_time / 60)  %% 60)
+my_time_seconds = floor(my_time %% 60)
 my_distance_km = sum(track$delta.dist) / 1000
 ##print(sum(track$delta.time)/60) + "minutes"
-paste("run duration:", my_time_minutes, "minutes ;","speed mean:" ,my_mean_speed, "km/h", "distance:",my_distance_km, "km" )
+
+
+paste("run duration:", my_time_hours,":", my_time_minutes, ":", my_time_seconds, "time;","speed mean:", my_mean_speed, "km/h", "speed max:", my_max_speed, "distance:",my_distance_km, "km", start )
+
+sprintf("start: %s",start_time)
+sprintf("finish: %s",finish_time)
+sprintf("run duration: %02d:%02d:%02d hours",my_time_hours,my_time_minutes,my_time_seconds)
+sprintf("run distance: %f km",my_distance_km)
+sprintf("run speed avg: %f km/h",my_mean_speed)
+
 
